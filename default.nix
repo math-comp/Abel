3df8e4448d4f3b3b541fbc69712f429723126d06
Cyril Cohen <cohen@crans.org>
2019-05-23 13:35:58 +0200

    setting up proper nix-shell + doc

--- default.nix
+++
@@ -1,8 +0,0 @@
-with import <mathcomp_update> {};
-
-stdenv.mkDerivation rec {
-  name = "env";
-  env = buildEnv { name = name; paths = buildInputs; };
-  buildInputs = [ coq ] ++ (with coqPackages; [mathcomp mathcomp-finmap]);
-}
-  
